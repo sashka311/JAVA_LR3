846623a77895c57280d66518cbf73c9d9abb874e
sashka311 <ihor.garashchenko@gmail.com>
2022-10-08 11:18:23 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("This task done by Ihor Garashchenko SE-19-1/9");
+    }
+}
