cf709ff27261585aa1e41930b1d349277c34632d
sashka311 <ihor.garashchenko@gmail.com>
2022-10-08 11:38:02 +0300

    completed lr3

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
-import java.util.Date;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("This task done by Ihor Garashchenko SE-19-1/9");
+        //1
+        char c2;
+        byte b;
+        float f;
+        int i = -500;
+        //2
+        Random random = new Random();
+        f = random.nextFloat(10000);
+        i = Math.round(f);
+        b = (byte)i;
+        System.out.println("f = "+f);
+        System.out.println("i = "+i);
+        System.out.println("b = "+b);
+        //3
+        double y = (Math.pow(i,5)/ Math.log(f)) - (Math.sin(f) / b);
+        System.out.println("y = "+y);
+
     }
 }
